7a9a2d75c43903f40f2d6869c26adf51bde5310b
Christos Gkoros <chistos.gkoros@vodafone.com>
2021-05-24 07:38:27 +0300

    email (userId) should be a property - externalize to app properties

--- src/main/java/org/cgoro/gmailarchiver/route/GmailRouteBuilder.java
+++ src/main/java/org/cgoro/gmailarchiver/route/GmailRouteBuilder.java
@@ -3,31 +3,32 @@ package org.cgoro.gmailarchiver.route;
 import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GmailRouteBuilder extends RouteBuilder {
+
+    //TODO: Check why userId=me is not working
+    @Value("${gmail.archiver.email}")
+    String userId;
+
     @Override
     public void configure() throws Exception {
 
-        from("google-mail:messages/list?userId=chr.goros@gmail.com&repeatCount=1")
-                .process(exchange -> {
-                    ListMessagesResponse list = exchange.getMessage().getBody(ListMessagesResponse.class);
-                    exchange.getIn().setBody(list.getMessages());
-                })
+        from("google-mail:messages/list?userId=" + userId + "&repeatCount=1")
+                .process(exchange -> exchange.getIn().setBody(exchange.getMessage().getBody(ListMessagesResponse.class).getMessages()))
                 .split().body().transform(simple("${body.id}"))
-                .toD("google-mail:messages/get?userId=chr.goros@gmail.com&id=${body}")
-                .process(exchange -> {
-                    exchange.getMessage().getBody(Message.class)
-                            .getPayload()
-                            .getHeaders().stream()
-                            .filter(header -> header.getName().equals("Subject"))
-                            .findFirst()
-                            .ifPresentOrElse(
-                                    messagePartHeader -> exchange.getIn().setBody("Subject: " + messagePartHeader.getValue()),
-                                    () -> exchange.getIn().setBody("Subject: -No Subject-")
-                            );
-                })
+                .toD("google-mail:messages/get?userId="+ userId + "&id=${body}")
+                .process(exchange -> exchange.getMessage().getBody(Message.class)
+                        .getPayload()
+                        .getHeaders().stream()
+                        .filter(header -> header.getName().equals("Subject"))
+                        .findFirst()
+                        .ifPresentOrElse(
+                                messagePartHeader -> exchange.getIn().setBody("Subject: " + messagePartHeader.getValue()),
+                                () -> exchange.getIn().setBody("Subject: -No Subject-")
+                        ))
                 .log("${body}");
     }
 }
